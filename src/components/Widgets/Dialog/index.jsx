9f88c4d27644f78746c0b5c330213afbab3b8dd9
Rui Yi Jiang <jiangruiyi@gmail.com>
2017-10-09 15:35:19 +0800

    refactor: create higher order draggable function for dialog

--- src/components/Widgets/Dialog/index.jsx
+++ src/components/Widgets/Dialog/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import PopupComponent from '../../PopupComponent';
 import Button from '../../Form/Button';
 import PropTypes from 'prop-types';
@@ -6,9 +8,108 @@ import { getCenterPosition , getClsName} from '../../../utils';
 import { zIndexs } from '../../../Constants';
 
 let zIndex = zIndexs.Dialog;
+// draggable
+function draggable(Component) {
+    class DraggableComponent extends Component {
+        init(props) {
+            super.init(props);
+            // handler
+            this.mouseupHandler = this.onMouseUp.bind(this);
+            this.mousemoveHandler = this.onMouseMove.bind(this);
+            this.mousedownHandler = this.onMouseDown.bind(this);
+            // state
+            this.state = {
+                left: 0,
+                top: 0
+            };
+        }
+
+        processRef(component) {
+            this.domNode = ReactDOM.findDOMNode(component);
+        }
+
+        componentWillReceiveProps(nextProps) {
+            super.componentWillReceiveProps(nextProps);
+
+            const { repositionOnOpen, isOpen} = this.props,
+                nextOpen = nextProps.isOpen === true && isOpen === false;
+
+            if (nextOpen && repositionOnOpen)  {
+                this.reposition();
+            }
+        }
+
+        componentDidMount() {
+            super.componentDidMount && super.componentDidMount();
+            this.reposition();
+        }
+
+        reposition() {
+            let {left, top} = getCenterPosition(window, this.domNode);
+            this.setState({left, top});
+        }
+
+        onMouseUp(e) {
+            document.removeEventListener('mousemove', this.mousemoveHandler);
+        }
+
+        onMouseMove(e) {
+            let ComponentBeingMoved = this.domNode,
+                rect = ComponentBeingMoved.getBoundingClientRect(),
+                {clientX, clientY} = e,
+                {lastMouseX, lastMouseY} = this,
+                leftToMouse = clientX - lastMouseX,
+                topToMouse = clientY - lastMouseY,
+                left = Math.max(0, this.state.left + leftToMouse),
+                top = Math.max(0, this.state.top + topToMouse);
+
+            this.setState({
+                left: Math.min(window.innerWidth - rect.width, left),
+                top: Math.min(window.innerHeight - rect.height, top)
+            });
+
+            this.lastMouseX = clientX;
+            this.lastMouseY = clientY;
+        }
+
+        onMouseDown(e) {
+            // only click on the header , we start draging the dialog
+            if (e.currentTarget !== e.target) return;
+            this.lastMouseX = e.clientX;
+            this.lastMouseY = e.clientY;
+            document.addEventListener('mousemove', this.mousemoveHandler);
+            document.addEventListener('mouseup', this.mouseupHandler);
+        }
+
+        render() {
+            const style = {
+                position: 'fixed',
+                left: this.state.left,
+                top: this.state.top
+            };
+
+            return (<Component 
+                        {...this.props} 
+                        ref={this.processRef.bind(this)}
+                        styleObj={style} 
+                        onMouseDown={this.mousedownHandler}
+                        onMouseUp={this.mouseupHandler} />);
+        }
+    }
+
+    DraggableComponent.defaultProps = Object.assign({
+        repositionOnOpen: true
+    }, Component.defaultProps);
+
+    DraggableComponent.propTypes = Object.assign({
+        repositionOnOpen: PropTypes.bool
+    });
+
+    return DraggableComponent;
+}
 
 // Dialog
-export default class Dialog extends PopupComponent {
+export class DialogView extends PopupComponent {
     init() {
         // class
         this.baseClassName = `dialog`;
@@ -22,39 +123,22 @@ export default class Dialog extends PopupComponent {
         this.onCancelBtnClicked = this.onCancelBtnClicked.bind(this);
         this.onConfirmBtnClicked = this.onConfirmBtnClicked.bind(this);
         this.onCloseBtnClicked = this.onCloseBtnClicked.bind(this);
-        this.mousemoveHandler = this.onMouseMove.bind(this);
-        this.mouseupHandler = this.onMouseUp.bind(this);
         // state
         this.state = {
-            left: 0,
-            top: 0,
             zIndex: zIndex++
         };
     }
 
-    componentDidMount() {
-        this.reposition();
-    }
-
-    reposition() {
-        let {left, top} = getCenterPosition(window, this.domNode);
-        this.setState({left, top});
-    }
-
-    componentWillReceiveProps(props) {
+    componentWillReceiveProps(nextProps) {
         const { open, close, repositionOnOpen, isOpen} = this.props,
-            nextOpen = props.isOpen === true && isOpen === false,
-            nextClose = props.isOpen === false && isOpen === true;
+            nextOpen = nextProps.isOpen === true && isOpen === false,
+            nextClose = nextProps.isOpen === false && isOpen === true;
 
         if (nextOpen)  {
             open({}, this);
             this.setState({
                 zIndex: zIndex++
             });
-
-            if (repositionOnOpen) {
-                this.reposition();
-            }
         }
 
         if (nextClose) {
@@ -85,51 +169,16 @@ export default class Dialog extends PopupComponent {
         }
     }
 
-    onMouseDown(e) {
-        // only click on the header , we start draging the dialog
-        if (e.currentTarget !== e.target) return;
-        this.lastMouseX = e.clientX;
-        this.lastMouseY = e.clientY;
-        document.addEventListener('mousemove', this.mousemoveHandler);
-        document.addEventListener('mouseup', this.mouseupHandler);
-    }
-
-    onMouseMove(e) {
-        let dialogBeingMoved = this.domNode,
-              rect = dialogBeingMoved.getBoundingClientRect(),
-              {clientX, clientY} = e,
-              {lastMouseX, lastMouseY} = this,
-              leftToMouse = clientX - lastMouseX,
-              topToMouse = clientY - lastMouseY,
-              left = Math.max(0, this.state.left + leftToMouse),
-              top = Math.max(0, this.state.top + topToMouse);
-
-        this.setState({
-            left: Math.min(window.innerWidth - rect.width, left),
-            top: Math.min(window.innerHeight - rect.height, top)
-        });
-
-        this.lastMouseX = clientX;
-        this.lastMouseY = clientY;
-    }
-
-    onMouseUp(e) {
-        document.removeEventListener('mousemove', this.mousemoveHandler);
-    }
-
     render() {
-        const {mod, title, body, cancelLabel, confirmLabel, closeBtnLabel} = this.props,
+        const {mod, title, body, cancelLabel, confirmLabel, closeBtnLabel, styleObj, onMouseDown, onMouseUp} = this.props,
                 cls = getClsName(this.className, mod, (!this.props.isOpen ? 'hidden' : '')),
-                style = {
-                    position: 'fixed',
-                    left: this.state.left,
-                    top: this.state.top,
+                css = Object.assign(styleObj, {
                     zIndex: this.state.zIndex
-                };
+                });
 
         return (
-            <div style={style} className={cls} ref={dialog => this.domNode = dialog}>
-                <div className={this.headerClass} onMouseDown={this.handleEvent} onMouseUp={this.handleEvent}>
+            <div style={styleObj} className={cls} >
+                <div className={this.headerClass} onMouseDown={onMouseDown} onMouseUp={onMouseUp} >
                     {title}
                     <Button title={closeBtnLabel} text={closeBtnLabel} onClicked={this.onCloseBtnClicked} className={this.closeBtnClass} />
                 </div>
@@ -145,7 +194,7 @@ export default class Dialog extends PopupComponent {
     }
 }
 
-Dialog.defaultProps = {
+DialogView.defaultProps = {
     mod: '',
     title: 'Dialog Title',
     body: 'ANR1032829 This is a sample message. Ddlfkweroiu sdfr sdfjeriu dfererdfsf.',
@@ -154,11 +203,12 @@ Dialog.defaultProps = {
     confirmLabel: 'Confirm',
     closeBtnLabel: 'Close',
     isOpen: false,
-    repositionOnOpen: true,
-    close: () => {}
+    open: () => {},
+    close: () => {},
+    styleObj: {},
 };
 
-Dialog.propTypes = {
+DialogView.propTypes = {
     mod: PropTypes.string,
     title: PropTypes.string,
     body: PropTypes.string,
@@ -166,12 +216,14 @@ Dialog.propTypes = {
     cancelLabel: PropTypes.string,
     confirmLabel: PropTypes.string,
     closeBtnLabel: PropTypes.string,
-    onClose: PropTypes.func,
     onCancel: PropTypes.func,
     onConfirm: PropTypes.func,
-    open: PropTypes.func,
+    open: PropTypes.func.isRequired,
     close: PropTypes.func.isRequired,
     isOpen: PropTypes.bool,
     zIndex: PropTypes.number,
-    repositionOnOpen: PropTypes.bool
+    styleObj: PropTypes.object
 };
+
+const Dialog = draggable(DialogView);
+export default Dialog;

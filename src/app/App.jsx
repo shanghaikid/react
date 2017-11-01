d10fa93b547ca526b966d2ffbdf33d6b329f840f
Rui Yi Jiang <jiangruiyi@gmail.com>
2017-11-01 10:53:21 +0800

    TODAY: test websocket/ws module

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -30,6 +30,19 @@ export default class App extends BaseComponent {
         });
 
         this.getMemoryStat();
+
+        // Create WebSocket connection.
+        const socket = new WebSocket('ws://localhost:8081');
+
+        // Connection opened
+        socket.addEventListener('open', function (event) {
+            socket.send('Hello Server!');
+        });
+
+        // Listen for messages
+        socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+        });
     }
 
     getMemoryStat() {
--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,13 @@ import logger from 'koa-logger';
 import root from './routes/root'
 console.info('Env:', process.env.NODE_ENV);
 
+import WebSocket from 'ws';
+
+
 // create koa server
 const app = new Koa(); 
+const wss = new WebSocket.Server({ port: 8081 });
+
 // use logger, routes
 app.use(logger())
     .use(async function (ctx, next) {
@@ -16,3 +21,13 @@ app.use(logger())
     .use(root.routes())
     .use(root.allowedMethods())
     .listen(3000);
+
+// TODO: combine ws with Koa for better and easy dev
+wss.on('connection', function connection(ws) {
+      ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+      });
+
+    ws.send('something');
+});
+
